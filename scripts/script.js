602086c220974bcc170139bbfe8367cb6a962dc2
Alexandre Rossetto <drossetto.alexandre@gmail.com>
2024-08-09 18:58:56 -0300

    finishing logic and style

--- scripts/script.js
+++ scripts/script.js
@@ -11,21 +11,25 @@ const clearAll = document.getElementById('clearAll')
 const icons = document.querySelectorAll("#icon")
 
 const resetValues = () => {
-   mortgageAmount.value = null
-   mortgageAmountErrorElement.innerText = ''
-   mortgageTerm.value = null
-   mortgageTermErrorElement.innerText = ''
-   interestRate.value = null
-   interestRateErrorElement.innerText = ''
-   mortgageType.forEach((radio) => {
-     radio.checked = false;
-   })
-   mortgageTypeErrorElement.innerText = ''
+  mortgageAmount.value = null
+  mortgageAmountErrorElement.innerText = ''
+  mortgageTerm.value = null
+  mortgageTermErrorElement.innerText = ''
+  interestRate.value = null
+  interestRateErrorElement.innerText = ''
 
-   icons.forEach((icon) => {
-     icon.style.color = 'black';
-     icon.style.background = 'var(--slate-100)';
-   })
+  mortgageType.forEach((radio) => {
+    radio.checked = false;
+  })
+  mortgageTypeErrorElement.innerText = ''
+  
+  icons.forEach((icon) => {
+    icon.style.color = 'black';
+    icon.style.background = 'var(--slate-100)';
+  })
+
+  document.getElementById('empty-results').style.display = 'flex';
+  document.getElementById('results').style.display = 'none';
 }
 
 const findSelected = () => {
@@ -48,19 +52,27 @@ const setColors = (event, field) => {
 const calcMortgage = () => {
   let monthlyPayment = 0;
   let totalRepayment = 0;
-
-  if(mortgageType.value === 'repayment') {
-    const monthlyRate = interestRate.value / 12
+  
+  const mortgageTypeElement = findSelected();
+  
+  if(mortgageTypeElement.value === 'Repayment') {
+    const rate = interestRate.value / 100
+    const monthlyRate = rate / 12
     const n = mortgageTerm.value * 12
 
-    monthlyPayment = (mortgageAmount * monthlyRate) / (1 - Math.pow((1 + monthlyRate), - n))
-    totalRepayment = monthlyPayment * n
-  } else if (mortgageType.value === 'interest') {
-    monthlyPayment = (amount * rate) / 12
-    totalRepayment = monthlyPayment * mortgageTerm.value * 12
+    console.log(interestRate.value)
+    console.log(mortgageTerm.value)
+
+    monthlyPayment = (mortgageAmount.value * monthlyRate) / (1 - Math.pow((1 + monthlyRate), 
+    - n));
+    totalRepayment = monthlyPayment * n;
+
+  } else if (mortgageTypeElement.value === 'Interest') {
+    monthlyPayment = (mortgageAmount.value * rate) / 12
+    totalRepayment = (monthlyPayment * mortgageTerm.value) * 12
   }
 
-  return { monthly: monthlyPayment , total: totalRepayment }
+  return { monthly: monthlyPayment.toFixed(2) , total: totalRepayment.toFixed(2) }
 };
 
 
@@ -74,15 +86,15 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const selectedRadio = findSelected();
 
-  if (mortgageAmount.value === '' || mortgageAmount.value === null) {
+  if (mortgageAmount.value === '' || mortgageAmount.value === null || mortgageAmount.value === 0) {
     mortgageAmountErrorElement.innerText = "This field is required"
     fields.push(0)
   }
-  if (mortgageTerm.value === '' || mortgageTerm.value === null) {
+  if (mortgageTerm.value === '' || mortgageTerm.value === null || mortgageTerm.value === 0) {
     fields.push(1)
     mortgageTermErrorElement.innerText = "This field is required"
   }
-  if (interestRate.value === '' || interestRate.value === null) {
+  if (interestRate.value === '' || interestRate.value === null || interestRate.value === 0) {
     fields.push(2)
     interestRateErrorElement.innerText = "This field is required"
   }
@@ -94,11 +106,14 @@ form.addEventListener('submit', (e) => {
   if(fields.length) setColors('error',fields);
 
   if(!fields.length && selectedRadio) {
-    const mortgage = calcMortgage()
+    const mortgage = calcMortgage();
 
-    console.log(mortgage)
+    document.getElementById('monthlyResult').innerHTML = `$ ${mortgage.monthly}`
+    document.getElementById('repayResult').innerHTML = `$ ${mortgage.total}`
+
+    document.getElementById('empty-results').style.display = 'none';
+    document.getElementById('results').style.display = 'flex';
   }
-
 })
 
 
